8189f70cb03f073277c376c39de02d7bd23fd3d7
ericksitocg <ericksitocg@hotmail.com>
2021-07-16 02:10:04 -0500

    Add: metodo iniciarSesion con Firebase,redireccionar a MenuActivity

--- app/src/main/java/com/example/parqueosinteligentes/MainActivity.java
+++ app/src/main/java/com/example/parqueosinteligentes/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.parqueosinteligentes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.app.VoiceInteractor;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -16,6 +21,11 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.json.JSONObject;
 
@@ -24,9 +34,59 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private EditText txt_email,txt_pass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        //Initializate Components
+        txt_email = (EditText) findViewById(R.id.editTextUsuario);
+        txt_pass = (EditText) findViewById(R.id.editTextPass);
     }
+
+    private void iniciarSesion(String email,String password){
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            String correo_usuario = user.getEmail();
+                                    Toast.makeText(MainActivity.this, "Sesión iniciada",
+                                    Toast.LENGTH_SHORT).show();
+                            toMenuView(correo_usuario);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Usuario o contraseña incorrecto.",
+                                    Toast.LENGTH_SHORT).show();
+                            txt_pass.setText("");
+                        }
+                    }
+                });
+
+    }
+
+    public void onClickIniciarSesion(View v){
+        String email = txt_email.getText().toString();
+        String pass = txt_pass.getText().toString();
+
+        iniciarSesion(email,pass);
+
+    }
+
+    public void toMenuView(String correo){
+        Intent intent= new Intent(this, Menu.class);
+        intent.putExtra("correo", correo);
+        startActivity(intent);
+    }
+
+
 }
